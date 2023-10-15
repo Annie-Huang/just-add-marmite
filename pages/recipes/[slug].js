f802bbe57e10cdf64d34080c6522e93a5fe861ad
Annie Huang <annie.hn@gmail.com>
2023-10-15 17:02:04 +1100

    Create the getStaticPaths method in the /pages/recipes/[slugs].js page

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,26 @@
+import { createClient } from 'contentful';
+
 export default function RecipeDetails() {
-  return (
-    <div>
-      Recipe Details
-    </div>
-  )
+  return <div>Recipe Details</div>;
 }
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: 'recipe',
+  });
+
+  const paths = res.items.map((item) => {
+    return { params: { slug: item.fields.slug } };
+  });
+
+  // If we get to a path that is not in the paths list, we will show 404 page rather than a fall back page.
+  return {
+    paths,
+    fallback: false,
+  };
+};
